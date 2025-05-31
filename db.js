b028ea3c950ace8a1290b2487d2626092f5330d4
Waqas56jb <hmza56jb@gmail.com>
2025-05-31 03:52:06 -0700

    froentend ready and i am trying to addd chatbot

--- db.js
+++ db.js
@@ -5,11 +5,15 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
-db.connect(err => {
-  if (err) throw err;
+db.connect((err) => {
+  if (err) {
+    console.error('Database connection failed:', err);
+    return;
+  }
   console.log('Connected to MySQL');
 });
 
